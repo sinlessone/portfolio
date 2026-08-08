ae48b1445c578618a9568b0805a9f31aade9fd28
moh <mohthegoat1@proton.me>
2026-08-08 15:34:58 +0100

    Enhance styling and layout with Tailwind CSS, add loading and not found components

---
+++ components/contacts.tsx
@@ -0,0 +1,101 @@
+"use client"
+import React, {useState} from 'react';
+import { FaJsSquare, FaLastfmSquare, FaPause, FaPlay, FaCheck, FaDiscord, FaGithub, FaNodeJs } from "react-icons/fa";
+import { SiTypescript, SiNamemc, SiAnilist, SiDiscorddotjs, SiTailwindcss } from "react-icons/si";
+import { RiNextjsFill } from "react-icons/ri";
+import { MdEmail } from "react-icons/md";
+import Link from "next/link";
+
+const Contacts = () => {
+    const [copiedD, setCopiedD] = useState(false);
+    const [copiedE, setCopiedE] = useState(false);
+
+    async function copyText(text: string) {
+        await navigator.clipboard.writeText(text);
+    }
+    return (
+        <div className={"flex flex-col gap-2 outline outline-[#1f1f21] h-full bg-[#131314] rounded-lg p-4 pt-2 items-center w-119"}>
+            <div className={"flex items-center justify-center"}>
+                <span className={"text-gray-400 text-lg"}>Contacts and Socials</span>
+            </div>
+            <div className={"flex flex-col items-center justify-center h-full"}>
+
+                <div className={"flex flex-row flex-wrap justify-center gap-3"}>
+                    {/*Github*/}
+                    <Link href={"https://github.com/sinlessone/"} target="_blank" rel="noopener noreferrer">
+                        <div className={"flex flex-row outline outline-[#1f1f21] px-2 py-1 rounded-sm gap-2 transition-colors hover:bg-[#212124] duration-300"}>
+                            <FaGithub className={"translate-y-1"}></FaGithub>
+                            Github
+                        </div>
+                    </Link>
+                    {/*Discord*/}
+                    <div className={"flex flex-row outline outline-[#1f1f21] px-2 py-1 rounded-sm transition-colors hover:bg-[#5865f2] duration-300"}>
+                        <button onClick={() => {
+                            copyText("sinlessone")
+                            setCopiedD(true)
+                            setTimeout(() => {
+                                setCopiedD(false);
+                            }, 1500)
+                        }}>
+                            <div className={`flex flex-row  items-center justify-center`}>
+                                <div className={`flex flex-row gap-1 transition-all duration-400 ${copiedD ? "opacity-0" : "opacity-100"}`}>
+                                    <FaDiscord className={`translate-y-1`}></FaDiscord>
+                                    <span className={""}>@sinlessone</span>
+                                </div>
+                                <div className={`absolute flex flex-row gap-1 transition-all duration-400 ${copiedD ? "opacity-100" : "opacity-0"}`}>
+                                    <FaCheck className={`translate-y-1`}></FaCheck>
+                                    <span className={""}>Copied!</span>
+                                </div>
+                            </div>
+                        </button>
+                    </div>
+                    {/*Email*/}
+                    <div className={"flex flex-row outline outline-[#1f1f21] px-2 py-1 rounded-sm transition-colors hover:bg-[#9a2ff2] duration-300"}>
+                        <button onClick={() => {
+                            copyText("mohisdad@proton.me")
+                            setCopiedE(true)
+                            setTimeout(() => {
+                                setCopiedE(false);
+                            }, 1500)
+                        }}>
+                            <div className={`flex flex-row items-center justify-center`}>
+                                <div className={`flex flex-row gap-1 transition-all duration-600 ${copiedE ? "opacity-0" : "opacity-100"}`}>
+                                    <MdEmail className={`translate-y-1`}></MdEmail>
+                                    <span className={""}>Email</span>
+                                </div>
+                                <div className={`absolute flex flex-row gap-1  transition-all duration-600 ${copiedE ? "opacity-100" : "opacity-0"}`}>
+                                    <FaCheck className={`translate-y-1`}></FaCheck>
+                                    <span className={""}>Copied!</span>
+                                </div>
+                            </div>
+                        </button>
+                    </div>
+                    {/*anilist*/}
+                    <Link href={"https://anilist.co/user/sinlessone/"} target="_blank" rel="noopener noreferrer">
+                        <div className={"flex flex-row outline outline-[#1f1f21] px-2 py-1 rounded-sm gap-2 transition-colors hover:bg-[#0b1622] duration-300"}>
+                            <SiAnilist className={"translate-y-1"}></SiAnilist>
+                            Anilist
+                        </div>
+                    </Link>
+                    {/*last.fm*/}
+                    <Link href={"https://www.last.fm/user/dtMoh/"} target="_blank" rel="noopener noreferrer">
+                        <div className={"flex flex-row outline outline-[#1f1f21] px-2 py-1 rounded-sm gap-2 transition-colors hover:bg-[#d1170e] duration-300"}>
+                            <FaLastfmSquare className={"translate-y-1"}></FaLastfmSquare>
+                            Last.fm
+                        </div>
+                    </Link>
+                    {/*namemc*/}
+                    <Link href={"https://namemc.com/profile/PureOfSin.1"} target="_blank" rel="noopener noreferrer">
+                        <div className={"flex flex-row outline outline-[#1f1f21] px-2 py-1 rounded-sm gap-2 transition-colors hover:bg-[#12161a] duration-300"}>
+                            <SiNamemc className={"translate-y-1"}></SiNamemc>
+                            Namemc
+                        </div>
+                    </Link>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Contacts;
---
+++ components/musicPlayer.tsx
@@ -0,0 +1,127 @@
+"use client"
+import React, {useRef, useState} from 'react';
+import Image from "next/image";
+import {Button} from "@/components/ui/button";
+import {FaPause, FaPlay} from "react-icons/fa";
+
+const MusicPlayer = () => {
+    const audioRef = useRef<HTMLAudioElement>(null);
+    const [playing, setPlaying] = useState(false);
+    const [currentTime, setCurrentTime] = useState(0);
+    const [clicked, setClicked] = useState(false);
+    const [duration, setDuration] = useState(0);
+
+    function handleSeek(e: React.ChangeEvent<HTMLInputElement>) {
+        const newTime = Number(e.currentTarget.value)
+        if (audioRef.current) {
+            audioRef.current.currentTime = newTime;
+            setCurrentTime(newTime);
+        }
+    }
+    function parseTime(time:number) {
+        const t = Math.floor(time) / 60
+        let s: number | string = Math.round((t % 1) * 60)
+        const m = Math.floor(t)
+        if (s < 10) {
+            s = s.toString().padStart(2, '0')
+        }
+        return `${m}:${s}`
+    }
+
+    function handlePlay() {
+        if (!audioRef.current) return
+        if (playing) {
+            audioRef.current.pause();
+            setPlaying(false);
+
+        } else {
+            audioRef.current.play();
+            setPlaying(true);
+        }
+    }
+
+    return (
+
+        <div className={"outline outline-[#1f1f21] w-[50%] rounded-lg bg-[#131314] p-4 pt-2 relative"}>
+            <button className={"absolute"} onClick={() => {
+                setClicked(true)
+                if (!audioRef.current) return
+
+                audioRef.current.play();
+                audioRef.current.volume = 0.4
+                setDuration(audioRef.current.duration)
+                if (!playing) {
+                    setPlaying(true);
+                }
+            }}>
+                <div className={`flex items-center justify-center h-screen w-screen fixed inset-0 bg-[#121212] z-2 transition duration-400 cursor-pointer ${clicked ? "opacity-0" : "opacity-100"} ${clicked ? "pointer-events-none" : "pointer-events-auto"}`}>
+                    <div className={"flex items-center justify-center w-[20%] h-[15%] outline outline-[#1f1f21] rounded-md"}>hi</div>
+                </div>
+            </button>
+
+
+            <div className={"flex flex-col items-center justify-center p-3 gap-10"}>
+                <div className={"flex flex-col items-center justify-center"}>
+                    <Image className={"rounded-full"} src={"/prom.jpg"} alt={"artwork"} width={64} height={64}></Image>
+                    <span className={"pt-2"}>Prom.</span>
+                </div>
+            </div>
+            <div>
+                <div className={"flex flex-row items-center justify-center"}>
+                    <div className={"flex flex-col items-center justify-center w-[90%] relative"}>
+                        <div className={"flex flex-row items-center justify-between absolute w-full -translate-y-6 text-gray-400 text-sm"}>
+                            <span>{parseTime(currentTime)}</span>
+                            <span>{parseTime(duration)}</span>
+
+                        </div>
+                        <input
+                            className={"w-full accent-purple-400"}
+                            type="range"
+                            autoComplete="off"
+                            min="0"
+                            max={duration}
+                            value={currentTime}
+                            onChange={handleSeek}
+                        />
+                    </div>
+                </div>
+                <div className={"flex flex-row items-center justify-center"}>
+                    <Button onClick={handlePlay}>
+                        {playing ? <div className={"flex flex-row items-center justify-center gap-1 p-1"}>
+                            <FaPause></FaPause>
+                            <span>Pause</span>
+                        </div> : <div className={"flex flex-row items-center justify-center gap-1 p-1"}>
+                            <FaPlay></FaPlay>
+                            <span>Play</span>
+                        </div>}
+
+                    </Button>
+                </div>
+
+            </div>
+            <audio
+                ref={audioRef}
+                src={"/prom.mp3"}
+
+                onTimeUpdate={(e) => setCurrentTime(e.currentTarget.currentTime)}
+                onLoadedMetadata={(e) => {
+                    console.log(e.currentTarget.duration)
+                    setDuration(e.currentTarget.duration);
+                }}
+                onDurationChange={(e) => {
+                    setDuration(e.currentTarget.duration);
+                }}
+                onPause={() => {
+                    setPlaying(false)
+                }}
+                onPlay={() => {
+                    setPlaying(true)
+                }}
+
+                onEnded={(e) => {setPlaying(false)}}
+            ></audio>
+        </div>
+    );
+};
+
+export default MusicPlayer;
