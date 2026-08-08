15c856cc7f1c8d0081b80771597cb842ed9ab94d
moh <mohthegoat1@proton.me>
2026-08-08 18:35:12 +0100

    Enhance not found page layout and styling for improved user experience

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,18 @@
 
 export default function notFound () {
     return (
-        <div className="flex justify-center items-center h-[calc(100dvh-25rem)]">
-            <div className="-bg-linear-74 from-black to-gray-950 w-200 h-100 rounded-md inset-y-50">
-                <div></div>
+        <div className="flex flex-col items-center justify-center h-screen">
+            <div className="flex flex-row items-center justify-center h-screen">
+                <div className={"outline outline-[#1f1f21] bg-[#131314] rounded-md w-200 h-100 "}>
+                    <div className="flex flex-row items-center justify-center p-8">
+                        <div className={"flex flex-col items-center justify-center p-8"}>
+                        <h1 className={"text-8xl -bg-linear-60 from-red-400 via-red-200 to-red-800 bg-clip-text text-transparent"}>4 0 4</h1>
+                            <span className={"text-4xl text-red-300 pb-12"}>Unknown Page</span>
+                            <span className={"text-2xl text-center"}>The page you&#39;re looking for does not exist, if you believe this is an error, please contact the site author</span>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
     )
