d8950422bfbf1030adc2fbb3748c26af531e8201
moh <mohthegoat1@proton.me>
2026-08-13 13:20:26 +0100

    Add Vercel Speed Insights integration for performance monitoring

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { FaJsSquare, FaNodeJs } from "react-icons/fa";
 import { SiTypescript, SiDiscorddotjs, SiTailwindcss } from "react-icons/si";
 import { RiNextjsFill } from "react-icons/ri";
 import MusicPlayer from "@/components/musicPlayer";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Contacts from "@/components/contacts";
 import TimeZone from "@/components/TimeZone";
 
@@ -12,6 +13,7 @@ import TimeZone from "@/components/TimeZone";
 export default function Home() {
     return (
         <div className={"flex items-center justify-center min-h-screen overflow-x-hidden p-4"}>
+            <SpeedInsights/>
             <div className={"flex flex-col items-center justify-center gap-4"}>
                     <div className={"flex flex-col md:flex-row columns-2 gap-4"}>
 
